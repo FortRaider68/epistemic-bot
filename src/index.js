413304bc69adf4c5d380b3b335ef5e832780a6ff
Pedro Rickson <62623930+FortRaider68@users.noreply.github.com>
2020-12-03 23:24:54 -0300

    remove unnecessary utils

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const JIMP = require('jimp')
 const Crypto = require('crypto')
-const BreakLinePhrase  = require('./utils/BreakLinePhrase');
-
-const limitCaracters = 36;
 
 const TmpDirectory = "tmp/Quotes"
 
@@ -11,14 +8,8 @@ async function CreateQuote(Phrase, src) {
     const image = await JIMP.read(src)
 
     const font = await JIMP.loadFont(JIMP.FONT_SANS_32_WHITE);
-
-    const Quotes = BreakLinePhrase(Phrase,limitCaracters)
-
-    Quotes.map(Quote => {
-      const pixelsY = 56 + Quotes.indexOf(Quote) * 39
-      const pixelsX = 33
-      image.print(font, pixelsX,pixelsY,Quote)
-    })
+    
+    await image.print(font,30,25,Phrase,620)
 
     Crypto.randomBytes(16,(err,bytes)=>{
       image.write(`${TmpDirectory}/${bytes.toString("HEX")}.png`)
